fd057f7b0ad7ebc83447d4ebcfb8ec56c63b03fa
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-14 12:41:47 +0200

    replacing methods with regular functions

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
-  </section>
-  <section class="container">
     <h2>{{ userName }}</h2>
     <h3>{{ userAge }}</h3>
+    <button @click="newAgeValue">Change Age</button>
+  </section>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <button @click="newAgeObj">Change Age</button>
   </section>
 </template>
 
 <script setup>
 import { reactive, ref } from "vue";
 
+// Values
+const userName = ref("Laura");
+const userAge = ref(31);
+
+// Objects
 const user = reactive({
   name: "Robert",
   age: 31,
 });
 
-const userName = ref("Laura");
-const userAge = ref(31);
+// Methods
+function newAgeValue() {
+  return userAge.value++;
+}
 
-console.log(user);
-
-setTimeout(function () {
-  user.name = "Rob";
-  user.age = 32;
-}, 2000);
-
-setTimeout(function () {
-  userName.value = "Lau";
-  userAge.value = 32;
-}, 2000);
+function newAgeObj() {
+  return user.age++;
+}
 </script>
 
 <style>
